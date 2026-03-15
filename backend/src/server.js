2d0f5954fc4d5d2c8fcde83fecaf39fd959b8288
NITISH YADAV <101405119+thisisnitishyadav@users.noreply.github.com>
2026-03-15 17:48:16 +0530

    url updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5001;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://ellieshairbeauty.com",
     credentials: true,
   })
 );
